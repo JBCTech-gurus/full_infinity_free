9e2a0aed7e8cb8f23f4a140195e30f577448042e
JBCTech-gurus <jbctech001@gmail.com>
2025-12-12 23:13:35 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ mkdir -p ./dist
 
 # Process PHP files and output HTML
 php index.php > ./dist/index.html
+php process_order.php > ./dist/process_order.html
 php success.php > ./dist/success.html
 # Add similar lines for other pages
 cp -r assets ./dist/assets
