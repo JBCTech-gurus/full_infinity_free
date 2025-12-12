b01841a3f9a4d7e4d2bb972ea14bea3e6814113d
JBCTech-gurus <jbctech001@gmail.com>
2025-12-12 22:35:39 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create the dist directory if it doesn't exist
+mkdir -p ./dist
+
+# Process PHP files and output HTML
+php index.php > ./dist/index.html
+php success.php > ./dist/success.html
+# Add similar lines for other pages
