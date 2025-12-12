c38d5ff3a62cd8f34b83ebb3afd9321efcf42709
JBCTech-gurus <jbctech001@gmail.com>
2025-12-12 22:59:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . 'inc/config.php'; $bundles = get_bundles(); ?>
+<?php require_once __DIR__ . '/inc/config.php'; $bundles = get_bundles(); ?>
 <!doctype html>
 <html lang="en">
 <head>
